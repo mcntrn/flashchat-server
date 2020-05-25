4147e237f4f4624884574f7a9d7e09e7c5d09301
Manuel Cintron <manuel.cintron@gmail.com>
2020-05-25 13:37:09 -0500

    Added basic logging

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV !== "PROD"){
 }
 const { ApolloServer } = require('apollo-server');
 const { PubSub } = require('apollo-server');
+const { ApolloLogExtension } = require('apollo-log');
 const { init, getMessages, createMessage } = require('./models/message')
 const { typeDefs } = require('./schema/gql-schema')
 const pubsub = new PubSub();
@@ -26,7 +27,9 @@ const resolvers = {
     },
 };
 
-const server = new ApolloServer({typeDefs, resolvers});
+const options = { level: process.env.LOG_LEVEL }
+const extensions = [() => new ApolloLogExtension(options)];
+const server = new ApolloServer({typeDefs, resolvers, extensions});
 
 init().then(() => {
     server.listen().then(({url}) => {
